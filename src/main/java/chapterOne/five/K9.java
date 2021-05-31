2c1744f40c3b413abe03dede3ab22aa638351e0b
gxx <1152862327@qq.com>
2021-05-31 16:21:59 +0800

    ...

---
+++ src/main/java/chapterOne/five/K9.java
@@ -0,0 +1,25 @@
+package chapterone.five;
+
+public class K9 {
+
+	public static void main(String[] args) {
+		/*
+		 *		----1--------
+		 *		| 	|		|
+		 *		0	3----	6
+		 *			|	|	|
+		 *			2	7	5----
+		 *					|	|
+		 *					4	9
+		 *					|
+		 *					8
+		 *
+		 *	This forest cannot be the result of running weighted quick-union.
+		 *
+		 *	Consider the tree composed of nodes 1, 6, 5, 4, 9, 8
+		 *	Its height is 4, which is higher than lg N
+		 *	lg 6 = 3 < 4
+		 *
+		 */
+	}
+}
